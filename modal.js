f94be66ac03b81807d3702024e84be35e6350c67
neverswood <nevers_wood@hotmail.fr>
2021-06-23 21:50:16 +0200

    Change class and id btn-fermer through btn-close

--- modal.js
+++ modal.js
@@ -14,7 +14,8 @@ const modalBottom = document.querySelectorAll(".bbottom");
 const formData = document.querySelectorAll(".formData");
 const close = document.getElementById("close");
 const form = document.getElementById("sectionForm");
-const fermer = document.getElementById("btn-fermer");
+const fermer = document.getElementById("btn-close");
+const message = document.getElementById("message");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -23,7 +24,8 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
   form.classList.remove("sectionFormClose");
-
+  fermer.style.visibility = "hidden";
+  message.style.visibility = "hidden";
 } 
 
 // Close modal event
@@ -56,49 +58,63 @@ form.addEventListener("submit", (e) => {
   const conditions = document.getElementById("checkbox1");
   let error = false;
 
-  if ( firstName.value.trim() < 2) {
+  if ( firstName.value.length < 2) {
     firstName.style.border = "2px solid red";
     document.getElementById("firstNameError").textContent = "Veuillez entrer 2 caractères minimum";
     error = true;
   } else {
-    firstName.style.border = "2px solid green";
+    firstName.style.border = "none";
     document.getElementById("firstNameError").textContent = "";
   }
 
-  if ( lastName.value.trim() < 2) {
+  if ( lastName.value.length < 2) {
     lastName.style.border =  "2px solid red";
     document.getElementById("lastNameError").textContent = "Veuillez entrer 2 caractères minimum";
     error = true;
+  } else {
+    lastName.style.border = "none";
+    document.getElementById("lastNameError").textContent = "";
   }
 
   if ( !email.value.match(emailRegex)) {
     email.style.border =  "2px solid red";
     document.getElementById("emailError").textContent = "Veuillez entrer un email valide";
     error = true;
+  } else {
+    email.style.border = "none";
+    document.getElementById("emailError").textContent = "";
   }
 
   if ( !birthday.value.match(birthdayRegex)) {
     birthday.style.border =  "2px solid red";
     document.getElementById("birthdateError").textContent = "Veuillez entrer une date valide";
     error = true;
+  } else {
+    birthdate.style.border = "none";
+    document.getElementById("birthdateError").textContent = "";
   }
 
-  if (!Number(quantity.value >=0 ) || !Number(quantity.value <= 99)) {
+  if (!Number(quantity.value >=0 ) || !Number(quantity.value <= 99)|| quantity.value == "") {
     quantity.style.border = "2px solid red";
     document.getElementById("quantityError").textContent = "Veuillez entrer un chiffre entre 0 et 99";
     error = true;
+  } else {
+    quantity.style.border = "none";
+    document.getElementById("quantityError").textContent = "";
   }
 
   if (!conditions.checked) {
     conditions.style.border =  "2px solid red";
     document.getElementById("checkboxError").textContent = "Veuillez cochez la case";
     error = true;
-  }
+  } 
 
   if (!error) {
     document.getElementById("sectionForm").className= "sectionFormClose";
-    document.getElementById("btn-fermer").className= "btn-fermer-visible";
+    document.getElementById("btn-close").className= "btn-close-visible";
+    fermer.style.visibility = "visible";
     document.getElementById("message").className= "message-thx";
+    message.style.visibility = "visible";
   }
 
 });
