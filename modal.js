aed3239b84e48005c6e3fc0ce6813d556669dd7e
neverswood <nevers_wood@hotmail.fr>
2021-10-05 14:48:27 +0200

    Delete position fixed of body when launchModal in modal.js

--- modal.js
+++ modal.js
@@ -20,21 +20,19 @@ const btnClose = document.getElementById("btn-close");
 const message = document.getElementById("message");
 const inputForm = document.querySelectorAll("text-control");
 
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-  bodystyle.style.position = "fixed";
   btnClose.style.visibility = "hidden";
   message.style.visibility = "hidden";
-  document.getElementById("sectionForm").className= "sectionFormOpen";
-} 
+  document.getElementById("sectionForm").className = "sectionFormOpen";
+}
 
 // Close modal event
-function closeModal () {          
+function closeModal() {
   modalbg.style.display = "none";
   bodystyle.style.position = "relative";
 }
@@ -45,14 +43,13 @@ close.addEventListener("click", closeModal);
 // Close modal when form is success
 btnClose.addEventListener("click", closeModal);
 
-
-// Validation 
+// Validation
 function addError(label, message) {
   document.getElementById(label).style.border = "2px solid red";
   document.getElementById(`${label}Error`).textContent = message;
 }
 
-function removeError (label) {
+function removeError(label) {
   document.getElementById(label).style.border = "none";
   document.getElementById(`${label}Error`).textContent = "";
 }
@@ -62,42 +59,47 @@ form.addEventListener("submit", (e) => {
   const firstName = document.getElementById("first");
   const lastName = document.getElementById("last");
   const email = document.getElementById("email");
-  const emailRegex = (/^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/);
-  const birthdayRegex = (/^\d{4}\-\d{1,2}\-\d{1,2}$/);
+  const emailRegex =
+    /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
+  const birthdayRegex = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
   const birthday = document.getElementById("birthdate");
   const quantity = document.getElementById("quantity");
   const conditions = document.getElementById("checkbox1");
   let error = false;
- 
-  if ( firstName.value.length < 2) {
+
+  if (firstName.value.length < 2) {
     addError("first", "Veuillez entrer 2 caractères minimum");
     error = true;
   } else {
     removeError("first");
   }
 
-  if ( lastName.value.length < 2) {
+  if (lastName.value.length < 2) {
     addError("last", "Veuillez entrer 2 caractères minimum");
     error = true;
   } else {
     removeError("last");
   }
 
-  if ( !email.value.match(emailRegex)) {
+  if (!email.value.match(emailRegex)) {
     addError("email", "Veuillez entrer un email valide");
     error = true;
   } else {
     removeError("email");
   }
 
-  if ( !birthday.value.match(birthdayRegex)) {
+  if (!birthday.value.match(birthdayRegex)) {
     addError("birthdate", "Veuillez entrer une date valide");
     error = true;
   } else {
     removeError("birthdate");
   }
 
-  if (!Number(quantity.value >=0 ) || !Number(quantity.value <= 99) || quantity.value === "") {
+  if (
+    !Number(quantity.value >= 0) ||
+    !Number(quantity.value <= 99) ||
+    quantity.value === ""
+  ) {
     addError("quantity", "Veuillez entrer un chiffre entre 0 et 99");
     error = true;
   } else {
@@ -112,11 +114,10 @@ form.addEventListener("submit", (e) => {
   }
 
   if (!error) {
-    document.getElementById("sectionForm").className= "sectionFormClose";
-    document.getElementById("btn-close").className= "btn-close-visible";
+    document.getElementById("sectionForm").className = "sectionFormClose";
+    document.getElementById("btn-close").className = "btn-close-visible";
     btnClose.style.visibility = "visible";
-    document.getElementById("message").className= "message-thx";
+    document.getElementById("message").className = "message-thx";
     message.style.visibility = "visible";
   }
-
 });
